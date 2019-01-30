3635349b6c06b9519954927ae3364a8ef8217562
yuri2 <yuri2peter@qq.com>
2019-01-31 02:09:19 +0800

    replace icon better Example passing port by hack koa router, cros, test page front axios test page debounce

--- server/app.js
+++ server/app.js
@@ -1,19 +1,25 @@
 const Koa = require('koa');
 const koaStatic = require('koa-static');
 const path = require('path');
+const cors = require('@koa/cors');
+const router = require('./router');
 
-module.exports = async function () {
-  const port = await require('get-port')({ port: 3001 });
+module.exports = async function server (preferPort = 80) {
+  const port = await require('get-port')({ port: preferPort });
   const app = new Koa();
+  app.use(cors()); // Enable cors with default options
+  router(app); // koa-router
   app.use(koaStatic(
     path.join( __dirname, './front')
   ));
   return new Promise(resolve => {
     app.listen(port, () => {
       const home = `http://127.0.0.1:${port}`;
+      const blank = `${home}/blank`;
       resolve({
         port,
         home,
+        blank,
       })
     });
   });
--- server/electron.js
+++ server/electron.js
@@ -1,29 +1,39 @@
-const { app, Menu, BrowserWindow } = require('electron');
+const { app, Menu, BrowserWindow, globalShortcut } = require('electron');
 const server = require('./app');
 const config = {
   width: 1200,
   height: 900,
-  openDevTools: false,
+  openDevTools: true,
   home: '',
 };
 
 (async () => {
   let mainWindow;
-  const { home } = await server();
+  const { home, blank, port } = await server(3001);
   app.on('ready', function createWindow () {
     Menu.setApplicationMenu(null);
     mainWindow = new BrowserWindow({ width: config.width, height: config.height });
-    mainWindow.loadURL(config.home || home);
-    if (config.openDevTools) {
-      // Open the DevTools.
-      mainWindow.webContents.openDevTools();
-    }
-
     mainWindow.on('closed', function () {
       mainWindow = null
-    })
-  });
+    });
+    const loadHome = () => { mainWindow.loadURL(config.home || home); };
+    const loadBlank = () => { mainWindow.loadURL(blank); };
+    loadBlank();
+    mainWindow.webContents.executeJavaScript(
+      `localStorage.setItem('reacoa', '${JSON.stringify({
+        port,
+        home,
+        electron: true,
+      })}')`,
+      true, loadHome
+    );
 
+    globalShortcut.register('CommandOrControl+F12', () => {
+      if (config.openDevTools) {
+        mainWindow.webContents.openDevTools();
+      }
+    });
+  });
   app.on('window-all-closed', function () {
     if (process.platform !== 'darwin') {
       app.quit()
