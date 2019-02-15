06af562326436a78fb8c18056523db90049ad5bd
yuri2peter <yuri2peter@qq.com>
2019-02-15 11:51:40 +0800

    ProtoTypes -> PropTypes

--- front/src/component/lib/Template/index.jsx
+++ front/src/component/lib/Template/index.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import withErrorBoundary from '../WithErrorBoundary';
 // import { observer } from 'mobx-react'
 // import classNames from 'classnames';
-// import ProtoTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 // import cssStyles from './index.module.scss';
 // import Model from './model.js';
 
