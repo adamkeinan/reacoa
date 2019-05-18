025f0d80f13c37e68a0ee27cc0d7bb0da94233d0
yuri2 <yuri2peter@qq.com>
2019-05-19 02:59:35 +0800

    raw-loader

--- front/config/webpack.config.js
+++ front/config/webpack.config.js
@@ -299,6 +299,10 @@ module.exports = function(webpackEnv) {
           // match the requirements. When no loader matches it will fall
           // back to the "file" loader at the end of the loader list.
           oneOf: [
+            {
+              loader: require.resolve('raw-loader'),
+              test: /\.(md|txt)$/i,
+            },
             // "url" loader works like "file" loader except that it embeds assets
             // smaller than specified limit in bytes as data URLs to avoid requests.
             // A missing `test` is equivalent to a match.
